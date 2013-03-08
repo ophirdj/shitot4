f870fdc4bcaa07e34eca32cc3a6cf20f405bb3ca
Daniel Eidel <azuren@hotmail.com>
2013-03-08 17:35:47 +0200

    intergration start

---
+++ shit4/tests/integrationTests/mainframeBackup/MainframeWindowStub.java
@@ -0,0 +1,115 @@
+package integrationTests.mainframeVotingStations;
+
+import global.dictionaries.Languages;
+import global.dictionaries.Messages;
+import global.gui.StationPanel;
+import partiesList.model.IPartiesList;
+import partiesList.model.IParty;
+import mainframe.gui.IMainframeWindow;
+import mainframe.logic.MainframeAction.MainframeState;
+
+/**
+ * the only actual use of this stub is the method 'showHistogram'
+ * that with its help we check whether the Mainframe sends the right list to
+ * the mentioned method
+ * we use the method 
+ * @author Emil
+ *
+ */
+public class MainframeWindowStub extends IMainframeWindow {
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * saves the parameter which the method 'showHistogram' gets as parameter
+	 */
+	private IPartiesList showHistogramParam;
+	
+	@Override
+	public Boolean getConfirmation(StationPanel station, String confirmationMessage) {
+		return null;
+	}
+
+	@Override
+	public void printError(StationPanel station, String errorMessage) {
+
+	}
+
+	@Override
+	public void printMessage(StationPanel station, String message) {
+
+	}
+
+	@Override
+	public void setLanguage(Languages language) {
+
+	}
+
+	@Override
+	public String translate(Messages message) {
+		return null;
+	}
+
+	@Override
+	public void printErrorMessage(Messages message) {
+
+	}
+
+	@Override
+	public void printInfoMessage(Messages message, IParty party) {
+
+	}
+
+	@Override
+	public void printInfoMessage(Messages message) {
+
+	}
+
+	@Override
+	public boolean printConfirmationMessage(Messages message) {
+		return false;
+	}
+
+	@Override
+	public boolean printConfirmationMessage(Messages message, IParty party) {
+		return false;
+	}
+
+	@Override
+	public void closeWindow() {
+
+	}
+
+	@Override
+	public int getID() {
+		return 0;
+	}
+	
+	/**
+	 * returns what 'showHistogram' got as parameter
+	 * @return the parameter which the method 'showHistogram' got
+	 */
+	public IPartiesList getWhatShowHistogramGot(){
+		return this.showHistogramParam;
+	}
+
+	@Override
+	public void setState(MainframeState state) {
+
+	}
+
+	@Override
+	public void displayHistogram() {
+	}
+
+	@Override
+	public void displayTable() {
+		
+	}
+
+	@Override
+	public void setDataDisplay(IPartiesList parties) {
+		this.showHistogramParam = parties;
+	}
+
+}
---
+++ shit4/tests/integrationTests/mainframeBackup/ReadSuppliedXMLFactoryIntStub.java
@@ -0,0 +1,58 @@
+package integrationTests.mainframeVotingStations;
+
+import fileHandler.logic.IReadSuppliedXML;
+import fileHandler.logic.ReadSuppliedXML;
+import partiesList.factories.IPartiesListFactory;
+import partiesList.factories.IPartyFactory;
+import votersList.factories.IVoterDataFactory;
+import votersList.factories.IVotersListFactory;
+
+/**
+ * implementation of the interface IReadSuppliedXMLFactory
+ * @author Emil
+ *
+ */
+public class ReadSuppliedXMLFactoryIntStub implements IReadSuppliedXMLFactory {
+
+	/**
+	 * the parties list factory
+	 */
+	private IPartiesListFactory partiesListFactory;
+	/**
+	 * the party factory
+	 */
+	private IPartyFactory partyFactory;
+	/**
+	 * the voters list factory
+	 */
+	private IVotersListFactory voterListFactory;
+	/**
+	 * the voter's data factory
+	 */
+	private IVoterDataFactory voterDataFactory;
+
+	/**
+	 * 
+	 * @param partiesListFactory the parties list factory
+	 * @param partyFactory the party factory
+	 * @param voterListFactory the voters list factory
+	 * @param voterDataFactory the voter's data factory
+	 */
+	public ReadSuppliedXMLFactory(IPartiesListFactory partiesListFactory,
+			IPartyFactory partyFactory, IVotersListFactory voterListFactory,
+			IVoterDataFactory voterDataFactory) {
+		this.partiesListFactory = partiesListFactory;
+		this.partyFactory = partyFactory;
+		this.voterListFactory = voterListFactory;
+		this.voterDataFactory = voterDataFactory;
+	}
+
+	@Override
+	public IReadSuppliedXML createInstance() {
+		String suppliedFolder = "IntegrationTests/mainframeBackup/";
+		return new ReadSuppliedXML(partiesListFactory, partyFactory,
+				voterListFactory, voterDataFactory, 
+				suppliedFolder+ "voters.xml", suppliedFolder + "votingRecords.xml");
+	}
+
+}
