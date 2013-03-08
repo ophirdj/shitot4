bd16ca02f39bc0a3e3224e9131bb2a97ea4b3061
Ophir De Jager <sophirdj@t2.technion.ac.il>
2013-03-08 13:02:51 +0200

    Edited the guides

--- shit4/src/practiceStation/guides/PracticeStationImagesEnglish.java
+++ shit4/src/practiceStation/guides/PracticeStationImagesEnglish.java
@@ -6,7 +6,7 @@ public class PracticeStationImagesEnglish implements IListImages{
 	final String src_directory = "Images/English/";
 	final String[] filesName ={"enterScreenEnglishExplained.png","ID_ScreenEnglishExplained.png",
 			"choosingScreenEnglishExplained1.png","choosingScreenEnglishExplained2.png",
-			"choosingScreenEnglishExplained3.png","ConformationEnglishExplained.png",
+			"choosingScreenEnglishExplained3.png","ConfirmationEnglishExplained.png",
 			"EndingScreenEnglishExplained.png"};
 	public File getFile(int num){
 		return new File(src_directory+filesName[num]);
--- shit4/src/practiceStation/guides/PracticeStationImagesHebrew.java
+++ shit4/src/practiceStation/guides/PracticeStationImagesHebrew.java
@@ -3,8 +3,11 @@ package practiceStation.guides;
 import java.io.File;
 
 public class PracticeStationImagesHebrew implements IListImages{
-	final String src_directory = "Images/English/";
-	final String[] filesName ={"enterScreenEnglishExplained.png","ID_ScreenEnglishExplained.png"};
+	final String src_directory = "Images/Hebrew/";
+	final String[] filesName ={"enterScreenExplainedHe.png","ID_ScreenExplainedHe.png",
+			"choosingScreenExplained1He.png","choosingScreenExplained2He.png",
+			"choosingScreenExplained3He.png","ConfirmationExplainedHe.png",
+			"EndingScreenExplainedHe.png"};
 	public File getFile(int num){
 		return new File(src_directory+filesName[num]);
 	}
